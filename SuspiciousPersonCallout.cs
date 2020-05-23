caee127b799525bbc522afaf3666847acfa4b66e
ktapia576 <ktapia576@hotmail.com>
2020-05-23 19:54:58 -0400

    Add project files.

---
+++ SuspiciousPersonCallout.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using CalloutAPI;
+using System.Runtime.InteropServices;
+
+namespace SuspiciousPersonCallout
+{
+    [GuidAttribute("24b727e4-697a-462c-b7ab-d38c6e4c5b67")]
+    [CalloutProperties("SuspiciousPerson", "FivePD", "1.0.0", Probability.Medium)]
+    public class SuspiciousPersonCallout : CalloutAPI.Callout
+    {
+    }
+}
